a2d23925c47626e5498c9308f96e9daa5889750b
Josh Hess <jhesski@gmail.com>
2018-10-27 19:06:12 -0400

    Updated banner to be responsive. Added navigation bar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.scss';
 
 class App extends Component {
   render() {
+	  const disabled = true;
     return (
 	    <React.Fragment>
-		    <header>&nbsp;</header>
+		    <header className={'top-header'}>&nbsp;</header>
+		    <header className={'main-banner'}>&nbsp;</header>
+		    <nav className={'main-navigation'}><span className={disabled ? 'link-disabled' : ''}>Art Work</span><span>Demo Reel</span><span>Resume</span></nav>
 	    </React.Fragment>
     );
   }
